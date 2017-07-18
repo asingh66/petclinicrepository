c0ab426b1c7f057fba9db7649c004f7cdb8dad52
dhana250 <dhanakosuri@Dhanas-MacBook-Pro.local>
2017-07-17 20:04:29 -0500

    new class added

---
+++ src/main/java/org/springframework/samples/petclinic/repository/VetAppointment.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.springframework.samples.petclinic.repository;
+
+/**
+ * @author dhanakosuri
+ *
+ */
+public class VetAppointment {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+}
